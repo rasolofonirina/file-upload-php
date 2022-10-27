b483ec5a6702d6d633bbf6782e8bb0bf5cf76061
Eric Rasolofonirina <ericseven.mg@gmail.com>
2022-10-27 18:41:41 +0200

    Rename image before upload.

--- process.php
+++ process.php
@@ -1,12 +1,19 @@
 <?php
-if (!empty($_POST['send']) && !empty($_FILES['fileToUpload'])) {
+if (!empty($_POST["send"]) && !empty($_FILES["fileToUpload"])) {
+    $parts = pathinfo($_FILES["fileToUpload"]["name"]);
+    $name = strtolower($parts["filename"]);
+    $extension = strtolower($parts["extension"]);
+
     $target_directory = "uploads/";
-    $target_file = $target_directory . basename($_FILES['fileToUpload']['name']);
+    $temp_name = $_FILES["fileToUpload"]["tmp_name"];
+    $file_size = $_FILES["fileToUpload"]["size"];
+
+    $new_name = sha1($name . microtime()) . "." . $extension;
+    $target_file = $target_directory . basename($new_name);
     $error = false;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES['fileToUpload']['tmp_name']);
+    $check = getimagesize($temp_name);
     if ($check !== false) {
         // echo $check['mime'];
         $error = false;
@@ -16,23 +23,23 @@ if (!empty($_POST['send']) && !empty($_FILES['fileToUpload'])) {
     }
 
     // Check if file already exists
-    if (file_exists($target_file)) {
+    if (file_exists($new_name)) {
         $error = true;
         echo 'Sorry, file already exists.';
     }
 
     // Check file size (in this case > 1 Mo)
-    if ($_FILES['fileToUpload']['size'] > (1024 * 1024)) {
+    if ($file_size > (1024 * 1024)) {
         $error = true;
         echo 'Sorry, your file is too large.';
     }
 
     // Allow certain file formats
     if (
-        $imageFileType != 'jpg'
-        && $imageFileType != 'png'
-        && $imageFileType != 'jpeg'
-        && $imageFileType != 'gif'
+        $extension != 'jpg'
+        && $extension != 'jpeg'
+        && $extension != 'png'
+        && $extension != 'gif'
     ) {
         $error = true;
         echo 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
@@ -43,9 +50,9 @@ if (!empty($_POST['send']) && !empty($_FILES['fileToUpload'])) {
         echo 'Sorry, your file was not uploaded.';
         // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+        if (move_uploaded_file($temp_name, $target_file)) {
             $error = false;
-            echo 'The file ' . htmlspecialchars(basename($_FILES['fileToUpload']['name'])) . ' has been uploaded.';
+            echo 'The file has been uploaded.';
         } else {
             echo 'Sorry, there was an error uploading your file.';
         }
