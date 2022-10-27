45821b00da8b40278ffebac1c19becb3136a8490
Eric Rasolofonirina <ericseven.mg@gmail.com>
2022-10-27 19:04:40 +0200

    Styling alerts with Bootstrap.

--- process.php
+++ process.php
@@ -19,19 +19,19 @@ if (!empty($_POST["send"]) && !empty($_FILES["fileToUpload"])) {
         $error = false;
     } else {
         $error = true;
-        echo 'File is not an image.';
+        $message = '<div class="alert alert-danger" role="alert">File is not an image.</div>';
     }
 
     // Check if file already exists
     if (file_exists($new_name)) {
         $error = true;
-        echo 'Sorry, file already exists.';
+        $message = '<div class="alert alert-danger" role="alert">Sorry, file already exists.</div>';
     }
 
     // Check file size (in this case > 1 Mo)
     if ($file_size > (1024 * 1024)) {
         $error = true;
-        echo 'Sorry, your file is too large.';
+        $message = '<div class="alert alert-danger" role="alert">Sorry, your file is too large.</div>';
     }
 
     // Allow certain file formats
@@ -42,21 +42,50 @@ if (!empty($_POST["send"]) && !empty($_FILES["fileToUpload"])) {
         && $extension != 'gif'
     ) {
         $error = true;
-        echo 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
+        $message = '<div class="alert alert-danger" role="alert">Sorry, only JPG, JPEG, PNG & GIF files are allowed.</div>';
     }
 
     // Check if there's no error
     if ($error === true) {
-        echo 'Sorry, your file was not uploaded.';
+        $message = '<div class="alert alert-danger" role="alert">Sorry, your file was not uploaded.</div>';
         // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($temp_name, $target_file)) {
             $error = false;
-            echo 'The file has been uploaded.';
+            $message = '<div class="alert alert-success" role="alert">The file has been uploaded.</div>';
         } else {
-            echo 'Sorry, there was an error uploading your file.';
+            $message = '<div class="alert alert-danger" role="alert">Sorry, there was an error uploading your file.</div>';
         }
     }
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Result</title>
+    </head>
+
+    <body class="d-flex align-items-center align-self-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 offset-md-4 text-center">
+                    <?= $message ?? ''; ?>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <script src="assets/js/app.js"></script>
+    </body>
+
+    </html>
+<?php
 } else {
     header('Location: index.php');
 }
